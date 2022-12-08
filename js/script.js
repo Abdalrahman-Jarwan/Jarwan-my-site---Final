ce7b18801de772672fe3235082bf0fdc9eb9ceea
blank477 <67621021+blank477@users.noreply.github.com>
2022-12-07 23:41:52 -0600

    trying to fix ham menu

--- js/script.js
+++ js/script.js
@@ -2,8 +2,13 @@
 
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
-
 }
+
+
+
+
+
+
 const root = document.documentElement;
 const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
 const marqueeContent = document.querySelector("ul.marquee-content");
