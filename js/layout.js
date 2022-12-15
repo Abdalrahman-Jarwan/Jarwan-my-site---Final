139ff11b96504f28f90907993fc0df22960e503e
blank477 <67621021+blank477@users.noreply.github.com>
2022-12-14 23:14:47 -0600

    changes

--- js/layout.js
+++ js/layout.js
@@ -1,16 +1,16 @@
-$(document).ready(function () {
-    loadNav();
-    loadFooter();
-});
+// $(document).ready(function () {
+//     loadNav();
+//     loadFooter();
+// });
 
-loadNav = () => {
-    $.get('../Nav&Fot/nav.html', (data) => {
-        $('body').prepend(data);
-    });
-}
+// loadNav = () => {
+//     $.get('../Nav&Fot/nav.html', (data) => {
+//         $('body').prepend(data);
+//     });
+// }
 
-loadFooter = () => {
-    $.get('../Nav&Fot/footer.html', (data) => {
-        $('body').append(data);
-    });
-}
+// loadFooter = () => {
+//     $.get('../Nav&Fot/footer.html', (data) => {
+//         $('body').append(data);
+//     });
+// }
